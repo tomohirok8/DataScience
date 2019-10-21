a717a98e13ebdd2a1ee1eb5b877c8bd025a30f51
tomohirok8 <50911712+tomohirok8@users.noreply.github.com>
2019-10-21 22:27:08 +0900

    グリッドサーチ（RBFカーネルSVM）のコード

--- ML/ML_grid_search.py
+++ ML/ML_grid_search.py
@@ -1,16 +1,18 @@
 from sklearn.svm import SVC
+import numpy as np
 
 
-def grid_search_SVM(x_train, y_train, x_test, y_test):
-    param_list = [0.001, 0.01, 0.1, 1, 10, 100]
+def grid_search_SVM_RBF(x_train, y_train, x_test, y_test):
+    param_list_gamma = list(np.arange(0,10,0.001))
+    param_list_C = list(np.arange(0,100,0.1))
     
     best_score = 0
     best_parameters = {}
-    for gamma in param_list:
-        for C in param_list:
-            svm = SVC(gamma=gamma, C=C)
-            svm.fit(x_train, y_train)
-            score = svm.score(x_test, y_test)
+    for gamma in param_list_gamma:
+        for C in param_list_C:
+            model = SVC(kernel='rbf', gamma=gamma, C=C)
+            model.fit(x_train, y_train)
+            score = model.score(x_test, y_test)
             print('gamma = ', gamma, 'C = ', C)
             print('score = ', score)
             # 最も良いスコアのパラメータとスコアを更新
