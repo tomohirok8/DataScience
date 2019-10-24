99e37605c30f7c42d5d043c94e5e286c3fa2f855
tomohirok8 <50911712+tomohirok8@users.noreply.github.com>
2019-10-24 18:10:25 +0900

    線形単回帰、ロジスティック回帰、k近傍法、サポートベクターマシン、ナイーブベイズ分類、決定木、ランダムフォレストのコード

--- ML/basic_ML.py
+++ ML/basic_ML.py
@@ -5,6 +5,9 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB
 from sklearn import metrics
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
 
 
 # 線形単回帰
@@ -12,22 +15,18 @@ def manual_Simple_regression(X, Y):
     X = np.vstack(X)
     # Xを[X 1]の形に
     X = np.array([[value,1] for value in X])
-    
     # 最小二乗法の計算を実行
     a, b = np.linalg.lstsq(X, Y)[0]
-    
     # 元のデータをプロット
     plt.plot(X, Y,'o')
     # 求めた回帰直線を表示
     plt.plot(X, a*X + b,'r')
-    
     # 結果のarrayを取得
     result = np.linalg.lstsq(X,Y)
     # 2つ目の要素が誤差の合計
     error_total = result[1]
     # 誤差の平均値の平方根を計算
     rmse = np.sqrt(error_total/len(X))
-    
     print('平均二乗誤差の平方根は、{:0.2f}'.format(rmse[0]))
 
 
@@ -35,7 +34,6 @@ def manual_Simple_regression(X, Y):
 def Multiple_regression(X, Y):
     lreg = LinearRegression()
     lreg.fit(X, Y)
-    
     print('切片の値は{:0.2f}'.format(lreg.intercept_))
     print('係数の数は{}個'.format(len(lreg.coef_)))
 
@@ -46,10 +44,8 @@ def Linear_Regression(x_train, x_test, y_train, y_test):
     lreg = LinearRegression()
     # 学習
     lreg.fit(x_train,y_train)
-    
     pred_train = lreg.predict(x_train)
     pred_test = lreg.predict(x_test)
-    
     print('x_trainを使ったモデルの平均二乗誤差＝{:0.2f}'.format(np.mean((y_train - pred_train) ** 2)))
     print('x_testを使ったモデルの平均二乗誤差＝{:0.2f}'.format(np.mean((y_test - pred_test) ** 2)))
 
@@ -57,29 +53,26 @@ def Linear_Regression(x_train, x_test, y_train, y_test):
 # ロジスティック回帰
 def Logistic_Regression(x_train, x_test, y_train, y_test):
     # インスタンス
-    log_model = LogisticRegression()
+    model = LogisticRegression()
     # 学習
-    log_model.fit(x_train, y_train)
+    model.fit(x_train, y_train)
     # モデルの精度を確認
-    log_model.score(x_train, y_train)
-    
-    log_model.coef_
-    
-    y_pred = log_model.predict(x_test)
-    
+    model.score(x_train, y_train)
+    model.coef_
+    y_pred = model.predict(x_test)
     print(metrics.accuracy_score(y_test, y_pred))
+    return model
 
 
 # k近傍法
 def k_NN(x_train, x_test, y_train, y_test, k):
     # インスタンス
-    knn = KNeighborsClassifier(n_neighbors=k)
+    model = KNeighborsClassifier(n_neighbors=k)
     # 学習
-    knn.fit(x_train,y_train)
-    
-    y_pred = knn.predict(x_test)
-    
+    model.fit(x_train,y_train)
+    y_pred = model.predict(x_test)
     print(metrics.accuracy_score(y_test, y_pred))
+    return model
 
 
 # サポートベクターマシン
@@ -88,43 +81,72 @@ def SVM_linear(x_train, x_test, y_train, y_test, C):
     model = SVC(kernel='linear', C=C)
     # 学習
     model.fit(x_train,y_train)
-    
     y_pred = model.predict(x_test)
-    
     print(metrics.accuracy_score(y_test, y_pred))
+    return model
 
 def SVM_RBF(x_train, x_test, y_train, y_test, gamma, C):
     # インスタンス作成
     model = SVC(kernel='rbf', gamma=gamma, C=C)
     # 学習
     model.fit(x_train,y_train)
-    
     y_pred = model.predict(x_test)
-    
     print(metrics.accuracy_score(y_test, y_pred))
+    return model
 
 def SVM_poly(x_train, x_test, y_train, y_test, degree, C):
     # インスタンス作成
     model = SVC(kernel='poly', degree=degree, C=C)
     # 学習
     model.fit(x_train,y_train)
-    
     y_pred = model.predict(x_test)
-    
     print(metrics.accuracy_score(y_test, y_pred))
+    return model
 
 
 # ナイーブベイズ分類
-def NB_classify(x_train, x_test, y_train, y_test):
+def GNB_classify(x_train, x_test, y_train, y_test):
     # インスタンス作成
     model = GaussianNB()
     # 学習
     model.fit(x_train,y_train)
-    
     y_pred = model.predict(x_test)
-    
     print(metrics.accuracy_score(y_test, y_pred))
+    return model
 
 
+# 決定木
+def Decision_Tree(x_train, x_test, y_train, y_test):
+    # インスタンス作成
+    model = DecisionTreeClassifier(max_depth=4,random_state=0)
+    # 学習
+    model.fit(x_train,y_train)
+    y_pred = model.predict(x_test)
+    print(metrics.accuracy_score(y_test, y_pred))
+    return model
 
 
+# ランダムフォレスト
+def Random_Forest(x_train, x_test, y_train, y_test):
+    # インスタンス作成
+    model = RandomForestClassifier(n_estimators=100,random_state=0)
+    # 学習
+    model.fit(x_train,y_train)
+    y_pred = model.predict(x_test)
+    print(metrics.accuracy_score(y_test, y_pred))
+    return model
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
