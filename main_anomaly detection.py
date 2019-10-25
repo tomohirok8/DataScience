a1ddef3339f718dcc348d44c59fc74771500dedc
tomohirok8 <50911712+tomohirok8@users.noreply.github.com>
2019-10-25 11:05:09 +0900

    異常検知のメインコード

--- main_anomaly detection.py
+++ main_anomaly detection.py
@@ -1,35 +1,36 @@
 import os
 os.chdir('D:\\GitHub\\datascience')
 os.getcwd()
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-from AD.anomal_detection import Local_Outlier_Factor, k_nearest_neighbor, one_class_SVM
-from data.read_data import read_data_01
+from AD.anomal_detection import Local_Outlier_Factor, k_nearest_neighbor, one_class_SVM, Isolation_Forest
+from data.read_data import read_data_01, make_random_XY
 
 #############################
 ####### データの読み込み #######
 #############################
 x_data, y_data = read_data_01()
-
+x_data, y_data = make_random_XY()
 
 #######################
 ####### 異常検知 #######
 #######################
-Local_Outlier_Factor(x_data, y_data, 5, 0.005)
-k_nearest_neighbor(x_data, y_data, 2, 0.05)
-one_class_SVM(x_data, y_data, 0.5)
-
+outlier_index = Local_Outlier_Factor(x_data, y_data, 5, 0.005)
+outlier_index = k_nearest_neighbor(x_data, y_data, 2, 0.05)
+outlier_index = one_class_SVM(x_data, y_data, 0.1)
+outlier_index = Isolation_Forest(x_data, y_data)
 
 ###########################
 ####### 異常値を特定 #######
 ###########################
+df_data = pd.DataFrame({'X': x_data,
+                        'Y': y_data})
 
+index_list = list(df_data.index)
 
-
-
+outlier_data = list(np.array(index_list)[outlier_index])
 
 
 
