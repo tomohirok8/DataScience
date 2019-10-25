fd8e9408be13cab30fe7a204fcc764e1ca669d46
tomohirok8 <50911712+tomohirok8@users.noreply.github.com>
2019-10-25 11:07:41 +0900

    学習用サンプルデータ（MNIST、Iris）読み込み、ランダムデータ作成コード

--- data/read_data.py
+++ data/read_data.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+import numpy as np
 from sklearn.datasets import load_iris
+from sklearn.datasets import make_blobs
 from keras.datasets import mnist
 from keras.utils import to_categorical
 
@@ -46,3 +48,82 @@ def read_data_Iris():
     sns.countplot('Petal Length',data=iris,hue='Species')
     
     return df_iris
+
+
+def make_data_blobs():
+    X, Y = make_blobs(n_samples=500, centers=4, random_state=8, cluster_std=2.4)
+    
+    #　表示
+    plt.figure(figsize=(11,7))
+    plt.scatter(X[:, 0], X[:, 1], c=Y, s=50, cmap='jet')
+    plt.grid()
+    plt.show()
+    
+    return X, Y
+    
+
+def read_data_01():
+    X = np.loadtxt('data/data_quality.txt', delimiter=',')
+    
+    # データプロット
+    plt.figure(figsize=(11,7))
+    plt.title('data01')
+    plt.scatter(X[:,0], X[:,1], marker='o', facecolors='none', edgecolors='black', s=80) 
+    x_min, x_max = X[:, 0].min()-1, X[:, 0].max()+1 
+    y_min, y_max = X[:, 1].min()-1, X[:, 1].max()+1
+    plt.xlim(x_min, x_max) 
+    plt.ylim(y_min, y_max) 
+    plt.xticks(()) 
+    plt.yticks(()) 
+    plt.show()
+    
+    return X[:,0], X[:,1]
+    
+    
+def make_random_sin():
+    # ダミーデータ生成
+    x_train = 10 * np.random.rand(100)
+    # サイン波にノイズをのせる関数
+    def sin_model(x, sigma=0.2):
+        noise = sigma * np.random.randn(len(x))
+        return np.sin(5 * x) + np.sin(0.5 * x) + noise
+    
+    # xからyを計算
+    y_train = sin_model(x_train)
+    
+    # 表示
+    plt.figure(figsize=(11,7))
+    plt.errorbar(x_train, y_train, 0.1, fmt='o')
+    
+    # テスト用データ生成
+    x_test = np.linspace(0, 10, 1000)
+    y_test = sin_model(x_test, 0)
+    
+    return x_train, y_train, x_test, y_test
+    
+
+def make_random_XY():
+    # ダミーデータ生成
+    X = 0.3 * np.random.randn(100, 2)
+    # 外れ値生成
+    ANOMALY_DATA_COUNT = 20
+    X_outliers = np.random.uniform(low=-4, high=4, size=(ANOMALY_DATA_COUNT, 2))
+    X = np.r_[X + 2, X - 2, X_outliers]
+    
+    return X[:,0], X[:,1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
