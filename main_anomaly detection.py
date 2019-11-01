1da383ee3c838f68cf5109adf7f217f9753dffe7
tomohirok8 <50911712+tomohirok8@users.noreply.github.com>
2019-11-01 13:24:02 +0900

    異常検知のメインコード

--- main_anomaly detection.py
+++ main_anomaly detection.py
@@ -3,25 +3,34 @@ os.chdir('D:\\GitHub\\datascience')
 os.getcwd()
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
-from AD.anomal_detection import Local_Outlier_Factor, k_nearest_neighbor, one_class_SVM, Isolation_Forest
-from data.read_data import read_data_01, make_random_XY
+from AD.anomal_detection import Local_Outlier_Factor, k_nearest_neighbor, one_class_SVM, Isolation_Forest, time_series_kNN, time_series_singular_spectrum
+from data.read_data import read_data_01, make_random_XY, electro_cardiogram
 
 
 #############################
 ####### データの読み込み #######
 #############################
+# 配列データ
 x_data, y_data = read_data_01()
 x_data, y_data = make_random_XY()
+
+# 時系列データ
+data = electro_cardiogram()
+
 
 #######################
 ####### 異常検知 #######
 #######################
+# 配列データ
 outlier_index = Local_Outlier_Factor(x_data, y_data, 5, 0.005)
 outlier_index = k_nearest_neighbor(x_data, y_data, 2, 0.05)
 outlier_index = one_class_SVM(x_data, y_data, 0.1)
 outlier_index = Isolation_Forest(x_data, y_data)
+
+# 時系列データ
+time_series_kNN(data)
+time_series_singular_spectrum(data)
 
 ###########################
 ####### 異常値を特定 #######
